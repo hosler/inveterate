08e224611ee31527de347f8275c05a6629bf4d43
Daniel Hosler <dhosler@cspire.net>
2021-09-25 14:51:24 -0500

    debug queues

--- site/core/tasks.py
+++ site/core/tasks.py
@@ -48,7 +48,7 @@ def calculate_inventory():
             inventory.save()
 
 
-@shared_task(base=Singleton, lock_expiry=60*15)
+@shared_task(lock_expiry=60*15)
 def provision_service(service_id, password):
     service = Service.objects.get(pk=service_id)
     proxmox = ProxmoxAPI(service.node.host, user=service.node.user, token_name='inveterate', token_value=service.node.key,
