bca2fade910e151e903f683e3a4ce5b2ef9ec5f9
hosler <danhosler@gmail.com>
2021-11-14 11:19:10 -0600

    remove session auth

--- site/app/settings.py
+++ site/app/settings.py
@@ -186,7 +186,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'app.auth.TokenAuthSupportQueryString',
     ],
