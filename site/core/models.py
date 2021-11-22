6e828c7f26cd8db0c33f8e989fca785bb31a73ff
hosler <danhosler@gmail.com>
2021-11-22 08:43:45 -0600

    show a pretty name for cluster too

--- site/core/models.py
+++ site/core/models.py
@@ -107,6 +107,9 @@ class Cluster(models.Model):
     key = models.CharField(max_length=255)
     type = models.CharField(max_length=255, default="proxmox", choices=NODE_CHOICES)
 
+    def __str__(self):
+        return self.name
+
 
 class Node(PlanBase):
     NODE_CHOICES = (
